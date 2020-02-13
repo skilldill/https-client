1d914751a4215bd08e0695be791982ef0bbc6955
Alexander Lomonosov <skilldill@yandex.ru>
2020-02-14 00:19:50 +0500

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { 
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton 
+} from '@ionic/react';
 import React from 'react';
 
 const Home: React.FC = () => {
@@ -6,18 +13,12 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>Skilldill App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
         The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <IonButton size="large" expand="block">Click me</IonButton>
       </IonContent>
     </IonPage>
   );
